af12d066ebdfab109c5a97dd29d31d2dc91712c7
SDaKko <danilkosenko172004@mail.ru>
2024-09-20 19:13:03 +0300

    Произведение цифр числа, не делящихся на пять

---
+++ lab1/not_div_on_five_mult.rb
@@ -0,0 +1,22 @@
+#Метод поиска произведения цифр числа, не делящихся на пять
+def not_div_on_five_mult(num)
+	mult = 1
+	while (num != 0)
+		digit = num % 10
+		num /= 10
+		is_div = false
+		if(digit % 5 != 0 && is_div == false)
+			mult *= digit
+			is_div = true
+		end
+	end
+	if(is_div == true)
+		return mult
+	else
+		return 0
+	end
+end
+
+puts "Введите число: "
+num = gets.to_i.abs
+puts "Произведение цифр числа, не делящихся на пять, равно #{not_div_on_five_mult(num)}"
