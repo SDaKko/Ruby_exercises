73ee1e145034382fe2d3f1d677cb7dcccb9196ab
SDaKko <danilkosenko172004@mail.ru>
2024-09-21 20:35:37 +0300

    Добавим параметры в формате ключ-значение

--- Students/main.rb
+++ Students/main.rb
@@ -1,9 +1,9 @@
 require_relative "class_student"
 
 students = [
-	Student.new("Иванов", "Владимир", "Аркадьевич"),
-	Student.new("Петров", "Андрей", "Геннадьевич", "12345"),
-	Student.new("Глушков", "Матвей", "Александрович"),
+	Student.new(name: "Иванов", surname: "Владимир", patronymic: "Аркадьевич"),
+	Student.new(name:"Петров", surname:"Андрей", patronymic:"Геннадьевич", id:"12345"),
+	Student.new(name:"Глушков", surname:"Матвей", patronymic:"Александрович", tg: "@telegram"),
 ]
 
 students.each do |student|
