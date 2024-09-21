9661e575a2f352580cbbb9d0129fef1970de0e5a
SDaKko <danilkosenko172004@mail.ru>
2024-09-21 23:13:19 +0300

    Добавили номера телефонов для проверки

--- Students/main.rb
+++ Students/main.rb
@@ -1,9 +1,9 @@
 require_relative "class_student"
 
 students = [
-	Student.new(name: "Иванов", surname: "Владимир", patronymic: "Аркадьевич"),
-	Student.new(name:"Петров", surname:"Андрей", patronymic:"Геннадьевич", id:"12345"),
-	Student.new(name:"Глушков", surname:"Матвей", patronymic:"Александрович", tg: "@telegram"),
+	Student.new(name: "Иванов", surname: "Владимир", patronymic: "Аркадьевич", phone: "+79183336640"),
+	Student.new(name:"Петров", surname:"Андрей", patronymic:"Геннадьевич", id:"12345", phone: "+79183336697"),
+	Student.new(name:"Глушков", surname:"Матвей", patronymic:"Александрович", tg: "@telegram", phone: "+70918333667"),
 ]
 
 students.each do |student|
