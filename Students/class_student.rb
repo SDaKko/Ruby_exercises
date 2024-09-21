b46c50b582ffd2dbab35762d9434712b85a2d4f5
SDaKko <danilkosenko172004@mail.ru>
2024-09-21 18:29:35 +0300

    Изменили конструктор
    Добавили возможность создания объектов с любой комбинацией заполнения необязательных полей.

--- Students/class_student.rb
+++ Students/class_student.rb
@@ -1,7 +1,7 @@
 class Student
 	attr_accessor :name, :surname, :patronymic, :id, :number, :tg, :email, :git
 
-	def initialize(surname, name, patronymic, id = nil, phone = nil, tg = nil, email = nil, git = nil)
+	def initialize(surname:, name:, patronymic:, id: nil, phone: nil, tg: nil, email: nil, git: nil)
 		@id = id
 		@surname = surname
 		@name = name
@@ -15,11 +15,11 @@ class Student
 	def to_s
 		str = "surname = #{@surname}, name = #{@name}, patronymic = #{@patronymic}"
 		str += ", id = #{@id}" if @id
-	    str += ", phone = #{@phone}" if @phone
-	    str += ", tg = #{@tg}" if @tg
-	    str += ", email = #{@email}" if @email
-	    str += ", git = #{@git}" if @git
-	    return str
+		str += ", phone = #{@phone}" if @phone
+		str += ", tg = #{@tg}" if @tg
+		str += ", email = #{@email}" if @email
+		str += ", git = #{@git}" if @git
+		return str
 	end
 
 end
