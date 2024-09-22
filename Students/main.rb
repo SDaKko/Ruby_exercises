a10edb00de9d9a0763174aa4afbeb7094cdfefa8
SDaKko <danilkosenko172004@mail.ru>
2024-09-22 23:10:02 +0300

    Проверяем валидацию

--- Students/main.rb
+++ Students/main.rb
@@ -1,11 +1,12 @@
 require_relative "class_student"
 
 students = [
-	Student.new(name: "Иванов", surname: "Владимир", patronymic: "Аркадьевич", phone: "+79183336640"),
-	Student.new(name:"Петров", surname:"Андрей", patronymic:"Геннадьевич", id:"12345", phone: "+79183336697"),
+	Student.new(name: "Иванов", surname: "Владимир", patronymic: "Аркадьевич", phone: "+79183336640", git: "github.com/ok"),
+	Student.new(name:"Петров", surname:"Андрей", patronymic:"Геннадьевич", id:"12345", phone: "+79183336697", tg: "@onehjd"),
 	Student.new(name:"Глушков", surname:"Матвей", patronymic:"Александрович", tg: "@telegram", phone: "+70918333667"),
 ]
 
 students.each do |student|
 	puts student, "\n"
+	puts(student.validate(), "\n")
 end
