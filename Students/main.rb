587eefbe1831bb1347b9e5ed6408c13811cb8696
SDaKko <danilkosenko172004@mail.ru>
2024-09-25 12:50:03 +0300

    Проверяем методы

--- Students/main.rb
+++ Students/main.rb
@@ -1,12 +1,15 @@
 require_relative "class_student"
 
 students = [
-	Student.new(name: "Иванов", surname: "Владимир", patronymic: "Аркадьевич", phone: "+79183336640", git: "github.com/ok"),
-	Student.new(name:"Петров", surname:"Андрей", patronymic:"Геннадьевич", id:"12345", phone: "+79183336697", tg: "@onehjd"),
-	Student.new(name:"Глушков", surname:"Матвей", patronymic:"Александрович", tg: "@telegram", phone: "+70918333667"),
+	Student.new(name: "Владимир", surname: "Иванов", patronymic: "Аркадьевич", phone: "+79183336640", git: "github.com/ok"),
+	Student.new(name:"Андрей", surname:"Петров", patronymic:"Геннадьевич", id:"12345", phone: "+79183336697", tg: "@onehjd"),
+	Student.new(name:"Матвей", surname:"Глушков", patronymic:"Александрович", tg: "@telegram", phone: "+70918333667"),
 ]
-
 students.each do |student|
 	puts student, "\n"
 	puts(student.validate(), "\n")
 end
+
+students[0].set_contacts(phone: "+77777777777")
+puts students[0].phone
+puts students[0].get_info
