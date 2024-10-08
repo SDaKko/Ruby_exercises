900a215d05a15ed5b343645b8236662e15136491
SDaKko <danilkosenko172004@mail.ru>
2024-10-08 22:36:36 +0300

    Внесены изменения в методы

--- Students/class_student.rb
+++ Students/class_student.rb
@@ -1,4 +1,4 @@
-class Main_Student
+class MainStudent
 	attr_reader :id, :surname_inits, :git, :contact 
 
 	def get_info
@@ -23,41 +23,74 @@ class Main_Student
 	end
 end
 
-class Student < Main_Student
+class Student < MainStudent
 	attr_accessor :name, :surname, :patronymic
 	attr_reader :phone, :tg, :email
 
-	def initialize(surname:, name:, patronymic:, id: nil, phone: nil, tg: nil, email: nil, git: nil)
-		if !Student.valid_surname?(surname)
+	def surname=(surname)
+		if (!surname.nil? && Student.valid_surname?(surname))
+			@surname = surname
+		else
 			raise ArgumentError, "Неверный формат фамилии: #{surname}"
-   		end
-   		if !Student.valid_name?(name)
+ 		end   
+    end
+    def name=(name)
+		if (!name.nil? && Student.valid_name?(name))
+			@name = name
+		else
 			raise ArgumentError, "Неверный формат имени: #{name}"
-   		end
-   		if !Student.valid_patronymic?(patronymic)
+ 		end   
+    end
+    def patronymic=(patronymic)
+		if (!patronymic.nil? && Student.valid_patronymic?(patronymic))
+			@patronymic = patronymic
+		else
 			raise ArgumentError, "Неверный формат отчества: #{patronymic}"
-   		end
-		if id && !Student.valid_id?(id)
+ 		end   
+    end
+    def id=(id)
+		if (Student.valid_id?(id))
+			@id = id
+		else
 			raise ArgumentError, "Неверный формат id: #{id}"
-   		end
-		if phone && !Student.valid_phone_number?(phone)
+ 		end   
+    end
+    def git=(git)
+		if (Student.valid_git?(git))
+			@git = git
+		else
+			raise ArgumentError, "Неверный формат адреса git: #{git}"
+ 		end   
+    end
+    private def phone=(phone)
+		if (Student.valid_phone?(phone))
+			@phone = phone
+		else
 			raise ArgumentError, "Неверный формат номера телефона: #{phone}"
-   		end
-   		if tg && !Student.valid_tg?(tg)
+ 		end   
+    end
+    private def tg=(tg)
+		if (Student.valid_tg?(tg))
+			@tg = tg
+		else
 			raise ArgumentError, "Неверный формат адреса телеграм: #{tg}"
-   		end
-   		if email && !Student.valid_email?(email)
+ 		end   
+    end
+    private def email=(email)
+		if (Student.valid_email?(email))
+			@email = email
+		else
 			raise ArgumentError, "Неверный формат адреса электронной почты: #{email}"
-   		end
-   		if git && !Student.valid_git?(git)
-			raise ArgumentError, "Неверный формат git: #{git}"
-   		end
-   		self.set_contacts(phone: phone, tg: tg, email: email)
-   		@id = id
-   		@surname = surname
-		@name = name
-		@patronymic = patronymic
-		@git = git
+ 		end   
+    end
+
+	def initialize(surname:, name:, patronymic:, id: nil, phone: nil, tg: nil, email: nil, git: nil)
+   		self.id = id
+   		self.surname = surname
+		self.name = name
+		self.patronymic = patronymic
+		self.git = git
+		self.set_contacts(phone: phone, tg: tg, email: email)
 	end
 
 	def to_s
@@ -70,39 +103,12 @@ class Student < Main_Student
 		return str
 	end
 
-	def self.is_git?(git)
-		if git == nil
-			return false
-		end
-		return true
-	end	
-
-	def self.is_phone_number?(phone)
-		if phone == nil
-			return false
-		end
-		return true
-	end	
-
-	def self.is_tg?(tg)
-		if tg == nil
-			return false
-		end
-		return true
-	end	
-
-	def self.is_email?(email)
-		if email == nil
-			return false
-		end
-		return true
-	end	
-
 	def self.valid_id?(id)
-		if id == nil
-			return false
+		if !id.nil?
+			return id.match?(/^[0-9]+$/)
+		else
+			return true
 		end
-		return id.match?(/^[0-9]+$/)
 	end
 
 	def self.valid_surname?(surname)
@@ -126,53 +132,97 @@ class Student < Main_Student
 		return patronymic.match?(/^[A-ZА-ЯЁ][a-zа-яё]+$/)
 	end
 
-	def self.valid_phone_number?(phone_number)
-		if !Student.is_phone_number?(phone_number)
-			return false
+	def self.valid_phone?(phone)
+		if !phone.nil?
+			return phone.match?(/^\+7\d{10}$/)
+		else
+			return true
 		end
-		return phone_number.match?(/^\+7\d{10}$/)
+
 	end
 
 	def self.valid_tg?(tg)
-		if !Student.is_tg?(tg)
-			return false
+		if !tg.nil?
+			return tg.match?(/^\@[a-zA-Z0-9_]{5,32}$/)
+		else
+			return true
 		end
-		return tg.match?(/^\@[a-zA-Z0-9_]{5,32}$/)
 	end
 
 	def self.valid_email?(email)
-		if !Student.is_email?(email)
-			return false
+		if !email.nil?
+			return email.match?(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z_-]+$/)
+		else
+			return true
 		end
-		return email.match?(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z_-]+$/)
 	end
+
 
 	def self.valid_git?(git)
-		if !Student.is_git?(git)
-			return false
+		if !git.nil?
+			return git.match?(/^(https:\/\/)?github.com\/[a-zA-Z0-9_-]+$/)
+		else 
+			return true
 		end
-		return git.match?(/^(https:\/\/)?github.com\/[a-zA-Z0-9_-]+$/)
 	end
 
-	def validate() 
-		return Student.is_git?(@git) && (Student.is_phone_number?(@phone) || Student.is_tg?(@tg) || Student.is_email?(@email))
+	def has_git?()
+		if self.git == nil
+			return false
+		end
+		return true
+	end	
+
+	def has_contact?()
+		if self.phone != nil
+			return true
+		end
+		if self.tg != nil
+			return true
+		end
+		if self.email != nil
+			return true
+		end
+		return false
+	end	
+
+	def validate()
+		return self.has_git?() && self.has_contact?()
 	end
 
 	def set_contacts(phone: nil, tg: nil, email: nil)
-		if Student.valid_phone_number?(phone)
-			@phone = phone
+		if !phone.nil?
+			self.phone = phone 
 		end
-		if Student.valid_tg?(tg)
-			@tg = tg
+		if !tg.nil?
+			self.tg = tg
+		end 
+		if !email.nil? 
+			self.email = email
+		end 
+	end
+
+	def get_fio
+		if !self.surname_inits.nil?
+			return "ФИО: #{self.surname_inits}"
 		end
-		if Student.valid_email?(email)
-			@email = email
+	end
+
+	def get_contact
+		if(!self.phone.nil?)
+			return "Телефон: #{self.phone}"
+		end
+		if(!self.email.nil?)
+			return "Почта: #{self.email}"
+		end
+		if(!self.tg.nil?)
+			return "Телеграм: #{self.tg}"
 		end
 	end
 
 end
 
-class Student_short < Main_Student
+class StudentShort < MainStudent
 	
 	def initialize(student: nil, id: nil, info: nil)
 		if(student)
