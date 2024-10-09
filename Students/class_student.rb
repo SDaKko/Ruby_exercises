28c5289189ec63559621c175913e13b44beeaddb
SDaKko <danilkosenko172004@mail.ru>
2024-10-09 13:03:27 +0300

    Расположили классы в файлах

--- Students/class_student.rb
+++ Students/class_student.rb
@@ -1,27 +1,4 @@
-class MainStudent
-	attr_reader :id, :surname_inits, :git, :contact 
-
-	def get_info
-		@surname_inits = "#{@surname} #{@name[0]}. #{patronymic[0]}."
-		str = "ФИО = #{surname_inits}"
-		if @git
-			str += ", git = #{git}"
-		end
-		contact = ""
-		if @phone
-			contact += ", phone = #{@phone}"
-			@contact = @phone
-		elsif @email
-			contact += ", email = #{@email}"
-			@contact = @email
-		elsif @tg
-			contact += ", tg = #{@tg}"
-			@contact = @tg
-		end
-		str += contact
-		return str
-	end
-end
+require_relative 'MainStudent.rb'
 
 class Student < MainStudent
 	attr_accessor :name, :surname, :patronymic
@@ -220,52 +197,4 @@ class Student < MainStudent
 		end
 	end
 
-end
-
-class StudentShort < MainStudent
-	
-	def initialize(student: nil, id: nil, info: nil)
-		if(student)
-			@id = student.id
-			@surname_inits = student.surname_inits
-			@git = student.git
-			@contact = student.contact
-		elsif (id && info)
-			@id = id
-			parse_str(info)
-		end	
-			
-	end
-
-	def parse_str(str)
-		str.split(",").map do |part|
-			piece = part.split("=")
-			case piece[0].strip
-			when "ФИО"
-				@surname_inits = piece[1].strip
-			when "git"
-				@git = piece[1].strip
-			when "phone"
-				@contact = piece[1].strip
-			when "tg"
-				@contact = piece[1].strip
-			when "email"
-				@contact = piece[1].strip
-			else
-				raise ArgumentError, "Неверный формат строки."
-			end
-		end
-	end
-
-	def get_info
-		str = "id = #{@id}"
-		str += ", ФИО = #{@surname_inits}"
-		if @git
-			str += ", git = #{@git}"
-		end
-		if @contact
-			str += ", контакт = #{@contact}"
-		end
-		return str
-	end
 end
