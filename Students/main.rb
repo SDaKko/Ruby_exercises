32c93391e68978c33815c0cb74e92d23d993609d
SDaKko <danilkosenko172004@mail.ru>
2024-10-09 22:10:58 +0300

    Добавили связи с файлами

--- Students/main.rb
+++ Students/main.rb
@@ -1,4 +1,5 @@
 require_relative "class_student"
+require_relative "StudentShort"
 
 students = [
 	Student.new(name: "Владимир", surname: "Иванов", patronymic: "Аркадьевич", phone: "+79183336640", git: "github.com/ok"),
@@ -7,13 +8,17 @@ students = [
 ]
 students.each do |student|
 	puts student, "\n"
-	puts(student.validate(), "\n")
+	puts(student.contain(), "\n")
 end
 
 students[0].set_contacts(phone: "+77777777777")
 puts students[0].phone
 puts students[0].get_info
 puts students[0].surname_inits
-st_short = Student_short.new(id: 1, info: students[0].get_info)
+st_short = StudentShort.new(id: "1", info: students[0].get_info)
 puts st_short.surname_inits
 puts st_short.contact
+puts st_short.get_info
+puts students[0].get_fio
+puts students[0].get_contact
+puts students[0].git
