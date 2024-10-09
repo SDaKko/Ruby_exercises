a6ec40d9b9c59f873ea8715e417c2caeeec3e414
SDaKko <danilkosenko172004@mail.ru>
2024-10-10 01:41:48 +0300

    Распределили методы по классам

--- Students/MainStudent.rb
+++ Students/MainStudent.rb
@@ -1,24 +1,59 @@
 class MainStudent
-	attr_reader :id, :surname_inits, :git, :contact 
+	attr_reader :id, :surname_inits, :git, :contact, :phone, :tg, :email
 
-	def get_info
-		@surname_inits = "#{@surname} #{@name[0]}. #{patronymic[0]}."
-		str = "ФИО = #{surname_inits}"
-		if @git
-			str += ", git = #{git}"
-		end
-		contact = ""
-		if @phone
-			contact += ", phone = #{@phone}"
-			@contact = @phone
-		elsif @email
-			contact += ", email = #{@email}"
-			@contact = @email
-		elsif @tg
-			contact += ", tg = #{@tg}"
-			@contact = @tg
-		end
-		str += contact
-		return str
+	def initialize(id: nil, git: nil)
+		self.id = id
+		self.git = git 
 	end
+
+	def id=(id)
+		if (MainStudent.valid_id?(id))
+			@id = id
+		else
+			raise ArgumentError, "Неверный формат id: #{id}"
+ 		end   
+    end
+
+    def git=(git)
+		if (MainStudent.valid_git?(git))
+			@git = git
+		else
+			raise ArgumentError, "Неверный формат адреса git: #{git}"
+ 		end   
+    end
+
+    def self.valid_id?(id)
+		if !id.nil?
+			return id.match?(/^[0-9]+$/)
+		else
+			return true
+		end
+	end
+
+	def self.valid_git?(git)
+		if !git.nil?
+			return git.match?(/^(https:\/\/)?github.com\/[a-zA-Z0-9_-]+$/)
+		else 
+			return true
+		end
+	end
+
+	def get_fio
+		if !self.surname_inits.nil?
+			return "ФИО: #{self.surname_inits}"
+		end
+	end
+
+	def get_contact
+		if(!self.phone.nil?)
+			return "Телефон: #{self.phone}"
+		end
+		if(!self.email.nil?)
+			return "Почта: #{self.email}"
+		end
+		if(!self.tg.nil?)
+			return "Телеграм: #{self.tg}"
+		end
+	end
+
 end
