e85629650bf580626b4522a92d9fb32d9eff4b9e
SDaKko <danilkosenko172004@mail.ru>
2024-10-13 22:31:00 +0300

    Добавили второй конструктор

--- Students/student_short.rb
+++ Students/student_short.rb
@@ -2,36 +2,40 @@ require_relative 'main_student.rb'
 
 class StudentShort < MainStudent
 	
-	def initialize(student: nil, id: nil, info: nil)
-		if(student)
-			super(id: student.id, git: student.git)
-			@surname_inits = student.surname_inits
-			@contact = student.contact
-		elsif (id && info)
-			super(id: id)
-			parse_str(info)
-		end	
-			
+	def initialize(student:, contact: nil)
+		super(id: student.id, git: student.git)
+		@surname_inits = "#{student.surname} #{student.name[0]}. #{student.patronymic[0]}."
+		@contact = student.phone || student.tg || student.email || contact
 	end
 
-	private def parse_str(str)
+	def self.from_str(id, str)
+
+		surname_inits = ""
+		git = ""
+		contact = ""
+
 		str.split(",").map do |part|
 			piece = part.split("=")
 			case piece[0].strip
 			when "ФИО"
-				@surname_inits = piece[1].strip
+				surname_inits = piece[1].strip
 			when "git"
-				@git = piece[1].strip
+				git = piece[1].strip
 			when "phone"
-				@contact = piece[1].strip
+				contact = piece[1].strip
 			when "tg"
-				@contact = piece[1].strip
+				contact = piece[1].strip
 			when "email"
-				@contact = piece[1].strip
+				contact = piece[1].strip
 			else
 				raise ArgumentError, "Неверный формат строки."
 			end
 		end
+
+		surname, name, patronymic = surname_inits.gsub('.', '').split(" ", 3)
+		student = Student.new(id: id, surname: surname, name: name, patronymic: patronymic, git: git)
+		student_sh = StudentShort.new(student: student, contact: contact)
+
 	end
 
 	def to_s
