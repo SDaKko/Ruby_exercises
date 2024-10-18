179c919eec1bbc01fda9135225a958316a674d68
SDaKko <danilkosenko172004@mail.ru>
2024-10-18 15:25:37 +0300

    Первая задача
    Нахождение элементов массива, расположенных перед последним минимальным.

--- blocks_collections/methods_with_blocks.rb
+++ blocks_collections/methods_with_blocks.rb
@@ -1,1 +1,14 @@
+def elements_before_last_min(arr)
+	return [] if arr.empty?
 
+	index_min = 0
+	min = arr[0]
+	arr.each_with_index do |elem, ind|
+		if elem <= min
+			index_min = ind
+			min = elem 
+		end
+	end
+
+	arr[0...index_min]
+end
