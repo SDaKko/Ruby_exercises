c94e1a0a92e55f270aa7cff28ae0ec0fb137bf69
SDaKko <danilkosenko172004@mail.ru>
2024-10-18 15:56:12 +0300

    Вторая задача
    Метод поиска в массиве элементов, расположенных после первого максимального.

--- blocks_collections/methods_with_blocks.rb
+++ blocks_collections/methods_with_blocks.rb
@@ -12,3 +12,18 @@ def elements_before_last_min(arr)
 
 	arr[0...index_min]
 end
+
+def elements_after_first_max(arr)
+	return [] if arr.empty?
+
+	index_max = 0
+	max = arr[0]
+	arr.each_with_index do |elem, ind|
+		if elem > max
+			index_max = ind
+			max = elem 
+		end
+	end
+
+	arr[index_max + 1..-1]
+end
