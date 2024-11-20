99a5d493375fde2e2fc59b1b458636c3d26bdd4e
SDaKko <danilkosenko172004@mail.ru>
2024-11-20 11:43:47 +0300

    Лаб4.Задание1.8 Протестировали работу методов
    Протестировали на примере формирования сущностей класса StudentShort.

--- Students/main.rb
+++ Students/main.rb
@@ -1,5 +1,6 @@
 require_relative "student"
 require_relative "student_short"
+require_relative 'data_list_student_short.rb'
 
 students = [
 	Student.new(name: "Владимир", surname: "Иванов", patronymic: "Аркадьевич", phone: "+79183336640", git: "github.com/ok"),
@@ -18,9 +19,14 @@ puts students[0].surname_inits
 st_short = StudentShort.from_str("1", students[0].get_info)
 st_short_last = StudentShort.new(student: students[1])
 puts st_short_last
-puts st_short.surname_inits
-puts st_short.contact
 puts st_short
-puts students[0].get_fio
-puts students[0].get_contact
-puts students[0].git
+
+data_table = DataTable.new([[]])
+data_list_1 = DataListStudentShort.new([st_short, st_short_last])
+data_list_1.select(0)
+data_list_1.select(1)
+united_table = data_list_1.unite_table()
+element = united_table.get_element(1, 1)
+puts element
+
+
