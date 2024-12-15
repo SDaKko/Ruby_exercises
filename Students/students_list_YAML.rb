b5eb20205b71d7ee1c223313dc06dbbf9c8bb6d5
SDaKko <danilkosenko172004@mail.ru>
2024-12-15 14:04:17 +0300

    Лаб4.Задание2.3 Класс StudentsListYAML

---
+++ Students/students_list_YAML.rb
@@ -0,0 +1,89 @@
+require_relative 'data_list_student_short.rb'
+require_relative 'student.rb'
+require_relative 'student_short.rb'
+require 'yaml'
+
+class StudentsListYAML
+	private attr_accessor :file_path
+	private attr_writer :students_array
+
+	def initialize(file_path)
+		self.file_path = file_path
+		self.students_array = read
+    end
+
+	def students_array()
+		@students_array.dup()
+	end
+
+	def read()
+		unless File.exist?(file_path)
+            raise "Файл не найден!" 
+        end
+
+		YAML.load_file(file_path, symbolize_names: true).map do |data|
+			Student.new(**data)
+		end
+	end
+
+	def write()
+		data = students_array.map { |student| student.to_h }
+		File.write(file_path, data.to_yaml)
+	end
+
+	def get_student_by_id(id)
+		students_array.each do |student|
+			if student.id == id
+				return student
+			end
+			raise ArgumentError, "id указан неверно!"
+		end
+	end
+
+
+	def get_k_n_student_short_list(k, n, data_list = nil)
+		start = (k - 1) * n
+		selected = []
+		if students_array[start, n]
+			selected = students_array[start, n]
+		end
+		students_short = selected.map { |student| StudentShort.new(student: student) }
+		if data_list.nil?
+			data_list = DataListStudentShort.new(selected)
+		end
+        
+		data_list
+	end
+
+	def sort_by_fio()
+		students_array.sort_by { |student| student.get_fio }
+	end
+
+	def add_student(student)
+		if students_array.nil?
+			max_id = 0
+		else
+			max_id = students_array.map{|student| student.id}.max
+		end
+		student.id = max_id + 1
+		@students_array << student
+	end
+
+	def replace_student_by_id(id, new_student)
+		index = students_array.find_index { |student| student.id == id }
+		unless index
+			raise IndexError, 'Неверно задан id студента!'
+		end
+		new_student.id = id
+		@students_array[index] = new_student
+	end
+
+	def delete_student_by_id(id)
+		@students_array.reject! { |student| student.id == id }
+	end
+    
+	def get_student_short_count()
+		students_array.count
+	end
+	
+end
