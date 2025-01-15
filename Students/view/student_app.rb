9030ad210a234f052cd2125dd93df6f942c424aa
SDaKko <danilkosenko172004@mail.ru>
2025-01-15 12:16:14 +0300

    Лаб5.Задание0.5 Создана область фильтрации

---
+++ Students/view/student_app.rb
@@ -0,0 +1,34 @@
+require 'fox16'
+require_relative 'student_list_view.rb'
+include Fox
+
+class StudentApp < FXMainWindow
+    def initialize(app)
+        super(app, "Students", width: 1024, height: 768)
+
+        tabs = FXTabBook.new(self, opts: LAYOUT_FILL)
+        FXTabItem.new(tabs, "Список студентов")
+        
+        student_list = FXVerticalFrame.new(tabs, opts: LAYOUT_FILL)
+        StudentListView.new(student_list)
+
+        FXTabItem.new(tabs, "Пустая вкладка")
+        second_tab = FXVerticalFrame.new(tabs, LAYOUT_FILL)
+        
+        FXTabItem.new(tabs, "Пустая вкладка")
+        third_tab = FXVerticalFrame.new(tabs)
+
+        FXVerticalFrame.new(tabs, opts: LAYOUT_FILL)
+    end
+    
+    def create
+        super
+        show(PLACEMENT_SCREEN)
+    end
+
+end
+
+app = FXApp.new("Students", "Students")
+StudentApp.new(app)
+app.create
+app.run
--- Students/view/student_list_view.rb
+++ Students/view/student_list_view.rb
@@ -1,30 +1,36 @@
 require 'fox16'
 include Fox
-class StudentApp < FXMainWindow
-    def initialize(app)
-        super(app, "Students", width: 1024, height: 768)
+class StudentListView < FXVerticalFrame
+    def initialize(parent)
+        super(parent, opts: LAYOUT_FILL)
+        setup_filtering_area
+    end
 
-        tabs = FXTabBook.new(self, opts: LAYOUT_FILL)
-        FXTabItem.new(tabs, "Список студентов")
-        student_list = FXVerticalFrame.new(tabs, opts: LAYOUT_FILL)
-
-        FXTabItem.new(tabs, "Пустая вкладка")
-        second_tab = FXVerticalFrame.new(tabs, LAYOUT_FILL)
-        
-        FXTabItem.new(tabs, "Пустая вкладка")
-        third_tab = FXVerticalFrame.new(tabs)
-
-        FXVerticalFrame.new(tabs, opts: LAYOUT_FILL)
+    def setup_filtering_area
+        filtering_area = FXVerticalFrame.new(self, opts: LAYOUT_FILL_X | LAYOUT_SIDE_TOP)
+        FXLabel.new(filtering_area, "Фильтрация")
+        FXHorizontalFrame.new(filtering_area, opts: LAYOUT_FILL_X) do |frame|
+            FXLabel.new(frame, "Фамилия и инициалы:")
+            FXTextField.new(frame, 20, opts: TEXTFIELD_NORMAL)
+        end
+        add_filtering_row(filtering_area, "Git:")
+        add_filtering_row(filtering_area, "Email:")
+        add_filtering_row(filtering_area, "Телефон:")
+        add_filtering_row(filtering_area, "Telegram:")
     end
     
-    def create
-        super
-        show(PLACEMENT_SCREEN)
+    def add_filtering_row(parent, label)
+        FXHorizontalFrame.new(parent, opts: LAYOUT_FILL_X) do |frame|
+            FXLabel.new(frame, label)
+            combo_box = FXComboBox.new(frame, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN)
+            combo_box.numVisible = 3
+            combo_box.appendItem("Не важно")
+            combo_box.appendItem("Да")
+            combo_box.appendItem("Нет")
+            text_field = FXTextField.new(frame, 15, opts: TEXTFIELD_NORMAL)
+            text_field.enabled = false
+            combo_box.connect(SEL_COMMAND){ text_field.enabled = (combo_box.currentItem == 1) }
+        end
     end
 
 end
-
-app = FXApp.new
-StudentApp.new(app)
-app.create
-app.run
